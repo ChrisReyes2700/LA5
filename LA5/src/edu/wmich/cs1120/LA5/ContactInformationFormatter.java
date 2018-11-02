98f64b5aebac43a7d7bd8b5e2842867b75eb15e5
Toby <Toby@tvpc>
2018-11-02 11:31:28 -0400

    Chris R,
    added interfaces and created all classes
    (still need to write code)

---
+++ LA5/src/edu/wmich/cs1120/LA5/ContactInformationFormatter.java
@@ -0,0 +1,25 @@
+package edu.wmich.cs1120.LA5;
+
+public class ContactInformationFormatter implements IContactInformationFormatter{
+	
+	public void readContactInformation(String[] filePaths) {
+		
+	}
+	
+	public void formatContactInformation(String fileName) {
+		
+	}
+	
+	public void formatEmail(String email) throws EmailAddressFormatException {
+		
+	}
+	
+	public void formatPhoneNumber(String phoneNumber) throws PhoneNumberFormatException {
+		
+	}
+	
+	public void formatName(String name) throws NameFormatException {
+		
+	}
+
+}
---
+++ LA5/src/edu/wmich/cs1120/LA5/FormatExceptionHandler.java
@@ -0,0 +1,22 @@
+package edu.wmich.cs1120.LA5;
+
+import java.io.FileNotFoundException;
+
+public class FormatExceptionHandler implements IFormatExceptionHandler{
+	
+	public void handleFileNotFoundException(FileNotFoundException e) {
+		
+	}
+	
+	public void handlePhoneNumberFormatException(PhoneNumberFormatException e) {
+		
+	}
+	
+	public void handleEmailFormatException(EmailAddressFormatException e) {
+		
+	}
+
+	public void handleNameFormatException(NameFormatException e) {
+		
+	}
+}
